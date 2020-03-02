8eb330aeda9f91e7fb5e20d6aee8e7253d1e398b
thelaim <thelaimm@gmail.com>
2020-03-02 11:38:31 +0300

    app

---
+++ menu/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Menu(models.Model):
+    menu_title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.menu_title
+
+class SubMenu(models.Model):
+	menu = models.ForeignKey(Menu, on_delete=models.CASCADE, blank=True, null=True)
+	parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+	sub_menu_title = models.CharField(max_length=64)
+
+	def __str__(self):
+		return self.parent
+
+	def __str__(self):
+		return self.sub_menu_title
+
